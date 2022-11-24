276b4505ffc9a2db21debb046b69a6cad34b618b
王玉东 <1174872613@qq.com>
2022-11-24 15:39:31 +0800

    增加spring web的context内容

--- src/main/java/com/test/demowyd/a02/A02.java
+++ src/main/java/com/test/demowyd/a02/A02.java
@@ -1,6 +1,59 @@
 package com.test.demowyd.a02;
 
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class A02 {
 
+    public static void main(String[] args) {
+        testAnnotationConfigServletWebServerApplicationContext();
+    }
+
+    private static void testAnnotationConfigServletWebServerApplicationContext(){
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+
+        for (String name : context.getBeanDefinitionNames()){
+            System.out.println(name);
+        }
+    }
+
+    @Configuration
+    static class WebConfig{
+
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory(){
+            return new TomcatServletWebServerFactory();
+        }
+
+        @Bean
+        public DispatcherServlet dispatcherServlet(){
+            return new DispatcherServlet();
+        }
+
+        @Bean
+        public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet){
+            return new DispatcherServletRegistrationBean(dispatcherServlet,"/");
+        }
+
+        @Bean("/helllo")
+        public Controller controller1(){
+            return ((request, response) -> {
+                response.getWriter().print("Hello!");
+                return null;
+            });
+        }
+
+    }
 
 }
