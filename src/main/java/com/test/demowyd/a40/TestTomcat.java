b26c51b51d9738dffd4ca1c5bf7bb946d75266ce
王玉东 <1174872613@qq.com>
2022-12-15 13:57:23 +0800

    tomcat2

--- src/main/java/com/test/demowyd/a40/TestTomcat.java
+++ src/main/java/com/test/demowyd/a40/TestTomcat.java
@@ -5,6 +5,17 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.coyote.http11.Http11Nio2Protocol;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
@@ -13,6 +24,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class TestTomcat {
@@ -47,12 +60,21 @@ public class TestTomcat {
         //3.创建Tomcat项目，在Tomcat中称为Context
         Context context = tomcat.addContext("",docBase.getAbsolutePath());
 
+        WebApplicationContext springContext = getApplicationContext();
+
         //4.编程添加Servlet
         context.addServletContainerInitializer(new ServletContainerInitializer() {
             @Override
             public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
                 HelloServlet helloServlet = new HelloServlet();
                 servletContext.addServlet("helloServlet",helloServlet).addMapping("/hello");
+
+//                DispatcherServlet dispatcherServlet = springContext.getBean(DispatcherServlet.class);
+//                servletContext.addServlet("dispatcherServlet",dispatcherServlet).addMapping("/");
+                for (ServletRegistrationBean registrationBean : springContext.getBeansOfType(ServletRegistrationBean.class).values()) {
+                    registrationBean.onStartup(servletContext);
+                }
+
             }
         }, Collections.emptySet());
 
@@ -64,6 +86,43 @@ public class TestTomcat {
         Connector connector = new Connector(new Http11Nio2Protocol());
         connector.setPort(8080);
         tomcat.setConnector(connector);
+
+    }
+
+    public static WebApplicationContext getApplicationContext(){
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(Config.class);
+        context.refresh();
+        return context;
+    }
+
+    @Configuration
+    static class Config{
+
+        @Bean
+        public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet){
+            return new DispatcherServletRegistrationBean(dispatcherServlet,"/");
+        }
+
+        @Bean
+        public DispatcherServlet dispatcherServlet(WebApplicationContext applicationContext){
+            return new DispatcherServlet(applicationContext);
+        }
+
+        @Bean
+        public RequestMappingHandlerAdapter handlerAdapter(){
+            RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+            handlerAdapter.setMessageConverters(List.of(new MappingJackson2HttpMessageConverter()));
+            return handlerAdapter;
+        }
+
+        @RestController
+        static class MyController{
+            @GetMapping("/hello2")
+            public Map<String,Object> hello(){
+                return Map.of("hello2","hello2,spring!");
+            }
+        }
 
     }
 
